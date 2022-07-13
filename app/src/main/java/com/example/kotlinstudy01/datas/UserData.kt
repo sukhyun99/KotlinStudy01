c8558fa0267dffdbd8d0c2693f3e0baae8233c62
sukhyun99 <buddah99@naver.com>
2022-07-13 18:58:47 +0900

    commit test

--- app/src/main/java/com/example/kotlinstudy01/datas/UserData.kt
+++ app/src/main/java/com/example/kotlinstudy01/datas/UserData.kt
@@ -5,10 +5,13 @@ import com.google.gson.annotations.SerializedName
 data class UserData(
     val id: String,
     val email: String,
+
     @SerializedName("nick_name")
     val nickname: String,
+
     @SerializedName("created_at")
     val createdAt: String,
+
     @SerializedName("updated_at")
     val updatedAt: String,
 )
